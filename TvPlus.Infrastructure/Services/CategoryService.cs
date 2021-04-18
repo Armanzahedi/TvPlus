3c85621d157a1f9b5a6788daf3f0bc75c543e805
Ali <a.jafarpour1372@gmail.com>
2021-04-18 16:40:25 +0430

    Category

--- TvPlus.Infrastructure/Services/CategoryService.cs
+++ TvPlus.Infrastructure/Services/CategoryService.cs
@@ -5,19 +5,51 @@ using AutoMapper;
 using TvPlus.Core.Models;
 using TvPlus.DataAccess;
 using TvPlus.DataAccess.Repositories;
+using TvPlus.Infrastructure.ViewModels;
 
 namespace TvPlus.Infrastructure.Services
 {
     public interface ICategoryService : ICategoryRepository
     {
+        Category Save(EditCategoryViewModel model);
+        EditCategoryViewModel GetCategoryForEdit(int id);
     }
     public class CategoryService : CategoryRepository, ICategoryService
     {
         private readonly ICategoryRepository _CategoryRepository;
-        public CategoryService(
-            ICategoryRepository CategoryRepository,MyDbContext context) : base(context)
+        private readonly IImageService _imageService;
+
+        public CategoryService(ICategoryRepository CategoryRepository, IImageService imageService, MyDbContext context) : base(context)
         {
             _CategoryRepository = CategoryRepository;
+            _imageService = imageService;
+        }
+
+        public Category Save(EditCategoryViewModel model)
+        {
+            var Category = new Category
+            {
+                Id = model.Id,
+                Title = model.Title,
+                ParentId = model.ParentId,
+                Show = model.Show,                
+            };
+            var savedCategory = base.AddOrUpdate(Category);
+            return savedCategory;
+        }
+
+
+        public EditCategoryViewModel GetCategoryForEdit(int id)
+        {
+            var category = base.GetById(id);
+            //var image = _imageService.GetByCenterId(id);
+            var vm = new EditCategoryViewModel
+            {
+                Id = category.Id,
+                Title = category.Title
+            };
+
+            return vm;
         }
     }
 }
---
+++ TvPlus.Infrastructure/ViewModels/CategoryViewModels.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using TvPlus.Infrastructure.Dtos.ContactUs;
+
+namespace TvPlus.Infrastructure.ViewModels
+{
+    public class CategoryGridViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class EditCategoryViewModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "عنوان")]
+        public string Title { get; set; }
+        //[Display(Name = "نام خانوادگی")]
+        //public string LastName { get; set; }
+        //[Display(Name = "توضیحات")]
+        //public string Description { get; set; }
+        //public string ImageName { get; set; }
+        [Display(Name = "دسته بندی")]
+        public int? ParentId { get; set; }
+        [Display(Name = "نمایش در منو سایت")]
+        public bool Show { get; set; }
+    }
+
+    public class EditCategoryValidator : AbstractValidator<EditCategoryViewModel>
+    {
+
+        public EditCategoryValidator()
+        {
+            RuleFor(sp => sp.Title).NotEmpty().WithName("عنوان");
+        }
+    }
+}
