07e33b12378fcea0948dab38d8d85642cb1a5f9d
Armanzahedi <Armanzdii@gmail.com>
2021-05-22 18:04:06 +0430

    Added User Crud

---
+++ TvPlus.Utility/StaticVariables.cs
@@ -0,0 +1,14 @@
+namespace TvPlus.Utility
+{
+    public static class StaticVariables
+    {
+        public const string SUPER_USER_ROLE_ID = "SADADSAD";
+
+        public const string SUPER_USER_ID = "75625814-138e-4831-a1ea-bf58e211acmf";
+
+        public const string ADMIN_ID = "75625814-138e-4831-a1ea-bf58e211adff";
+        public const string ADMIN_ROLE_ID = "29bd76db-5835-406d-ad1d-7a0901447c18";
+        public const string USER_ROLE_ID = "d7be43da-622c-4cfe-98a9-5a5161120d24";
+        public const int UserDefaultPassword_sys_id = 4;
+    }
+}
